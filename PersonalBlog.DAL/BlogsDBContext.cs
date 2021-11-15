ba1707d5067052631a27f387abd0049bc9d4dd79
Dmytro Bezukh <dimabezuh0611@gmail.com>
2021-11-15 10:55:29 +0200

    UoW and db context

---
+++ PersonalBlog.DAL/BlogsDBContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalBlog.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalBlog.DAL
+{
+    public class BlogsDBContext : IdentityDbContext<UserWithIdentity, IdentityRole, string>
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public BlogsDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.UserWithIdentity)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserWithIdentityId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Blog>().HasQueryFilter(p => !p.IsDeleted);
+            builder.Entity<Comment>().HasQueryFilter(p => !p.IsDeleted);
+            builder.Entity<Tag>().HasQueryFilter(p => !p.IsDeleted);
+            builder.Entity<Article>().HasQueryFilter(p => !p.IsDeleted);
+        }
+    }
+
+}
---
+++ PersonalBlog.DAL/UnitOfWork.cs
@@ -0,0 +1,71 @@
+﻿using PersonalBlog.DAL.Interfaces;
+using PersonalBlog.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BlogsDBContext _blogsDBContext;
+
+        private IBlogRepository blogRepository;
+        private IArticleRepository articleRepository;
+        private ITagRepository tagRepository;
+        private ICommentRepository commentRepository;
+
+        public UnitOfWork(BlogsDBContext blogsDBContext)
+        {
+            _blogsDBContext = blogsDBContext;
+        }
+
+        public IBlogRepository BlogRepository
+        {
+            get
+            {
+                if (blogRepository == null)
+                    blogRepository = new BlogRepository(_blogsDBContext);
+                return blogRepository;
+            }
+        }
+
+
+        public IArticleRepository ArticleRepository
+        {
+            get
+            {
+                if (articleRepository == null)
+                    articleRepository = new ArticleRepository(_blogsDBContext);
+                return articleRepository;
+            }
+        }
+
+        public ICommentRepository CommentRepository
+        {
+            get
+            {
+                if (commentRepository == null)
+                    commentRepository = new CommentRepository(_blogsDBContext);
+                return commentRepository;
+            }
+        }
+
+        public ITagRepository TagRepository
+        {
+            get
+            {
+                if (tagRepository == null)
+                    tagRepository = new TagRepository(_blogsDBContext);
+                return tagRepository;
+            }
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _blogsDBContext.SaveChangesAsync();
+        }
+    }
+
+}
